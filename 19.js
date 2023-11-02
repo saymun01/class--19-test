4c7993f3c9d744cb1b3e2e54e2dcc88f409f2f37
Saymun Nawaz <145199539+saymun01@users.noreply.github.com>
2023-11-02 10:53:07 +0600

    Add files via upload

---
+++ 19.js
@@ -0,0 +1,105 @@
+// let product = 'pant';
+// product = 'shirt';
+// console.log(product);
+// const website = 'www.eshion.com';
+// website = 'shikhon.com';
+// console.log(website);
+
+// const numbers = ['candan', 143, false, 'player']
+
+// numbers.unshift('ali');
+// numbers.shift()
+
+// console.log(numbers.indexOf(2));
+
+// console.log(numbers);
+
+// const student ={
+//     name: 'aliShorif',
+//     id: 143,
+//     dress: 'white'
+// };
+// student.dress ='black dress';
+// student['id'] = 134
+// console.log(student);
+
+{/* < > == !<=  =<   */}
+
+// if else
+// let item = 0;
+// while (item > 10){
+//     console.log(item);
+//     item++
+// }
+
+// for(let i =0; i <= 0; i++) {
+//     i++
+// }
+
+// function addTwoNumber (one, two) {
+//     const total = one + two;
+//     return total
+// }
+
+// const result = addTwoNumber(400,500)
+
+// const myname = '1.8989';
+// parseInt(myname)
+// parseFloat(numbers)
+
+// let, Const Concept
+
+// Inch To Feet Convert By Javascript
+// function inchToFeet(inch){
+//     const feetFesult = inch / 12
+//     return feetFesult
+// }
+
+// const output = inchToFeet(120)
+// console.log(output);
+
+
+// 4 % 2 == 0
+// 4 % 5 == 0
+// if (4 % 3 == 0) {
+//     console.log('good');
+// }
+
+// function isLeepYear(year){
+//     if (year % 4 == 0) {
+//         console.log('This year is not leep year');
+//     }
+//     else {
+//         console.log('This year is not leep year');
+//     }
+// }
+
+// isLeepYear(2024)
+
+// function milesToKilomitters(miles) {
+//     const Kilometres = miles * 1.60934
+//     return Kilometres;
+
+// }
+
+// const result = milesToKilomitters(50);
+// console.log(result, 'Kilomitters');
+
+
+// isLeepYear(2024)
+
+
+// function isEven(num) {
+//     if (num == 0) {
+//         return '0 is not right'
+//     }
+//     if(num % 2 == 0){
+//         return "This is Even Number"
+//     }
+//     else {
+//         return 'This is not Even number'
+//     }
+// }
+
+// const output2 = isEven(3);
+// console.log(output2);
